f51aff754f2fcecf8a7d39f2e75cc7b1bc794b3d
joyshjj12 <joyshjj1234@gmail.com>
2025-11-12 17:31:50 +0530

    fourth commit

--- app.py
+++ app.py
@@ -47,6 +47,6 @@ def home():
     return render_template_string(HTML_FORMAT)
 
 if __name__ == "__main__":
-    app.run(hosts='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000)
 
 # i am doing my second commit
